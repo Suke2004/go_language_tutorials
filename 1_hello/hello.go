f288a3cd685987fb185c184a772d17f28138b87f
Suke2006 <lyricsofsongs96@gmail.com>
2024-12-15 23:23:40 +0530

    hello

---
+++ 1_hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main (){
+	num :=10
+	name := "Santhu"
+	fmt.Println("Hello")
+	fmt.Printf("hello %s %T",name, num)
+	fmt.Printf("Helo world %s",name)
+	fmt.Printf("I got new keyboard")
+}
